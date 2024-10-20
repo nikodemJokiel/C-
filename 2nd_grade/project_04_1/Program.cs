dff44e17e858d8668aa9a487cb8bf35d6f90c889
Nikodem Jokiel <144921134+nikodemJokiel@users.noreply.github.com>
2024-10-20 14:48:52 +0200

    Add files via upload

---
+++ 2nd_grade/project_04_1/Program.cs
@@ -0,0 +1,91 @@
+﻿using System.Threading.Channels;
+
+namespace project_4_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //przykłady typów predefiniowanych
+            string message = "iMiĘ i NazWISkO";
+            Console.WriteLine(message);
+
+            string messageUpper = message.ToUpper();
+            Console.WriteLine(messageUpper); //IMIĘ I NAZWISKO
+
+            string newMessage = message[0].ToString().ToUpper() + message.Substring(1).ToLower();
+            Console.WriteLine(newMessage); //Imię i nazwisko
+
+
+            string number = "1234567890";
+            Console.WriteLine(number);
+            Console.WriteLine(number.Substring(3, 3)); //456
+            Console.WriteLine(number.Substring(0, 5)); //12345
+
+            //zakres zmiennej
+            Console.WriteLine(int.MinValue); //-2147483648
+            Console.WriteLine(int.MaxValue); //2147483647
+            Console.WriteLine(char.MaxValue); //?
+
+            string firstName = "Kamil";
+            Console.WriteLine(firstName.Length); //5
+            Console.WriteLine("Bąk".Length); //3
+
+            Console.WriteLine(firstName[0]); //K
+
+            string lastName = "Nowak";
+            Console.WriteLine(firstName.Equals(lastName)); //False
+            Console.WriteLine("Janusz".Equals("janusz")); //False
+            Console.WriteLine("janusz".Equals("janusz")); //True
+
+            Console.Clear();
+
+            Console.Write("Podaj imię: ");
+            string? name = Console.ReadLine();
+            Console.WriteLine($"Imię: {name}");
+
+            /*
+            while (true)
+            {
+                Console.Write("Podaj nazwisko: ");
+                string? lastName2 = Console.ReadLine();
+                if(lastName2 != string.Empty)
+                {
+                    Console.WriteLine($"Nazwisko: {lastName2}");
+                    break;
+                }
+            }
+           */
+
+            Console.Write("Podaj nazwisko: ");
+            string? lastName2 = Console.ReadLine();
+
+            while ( lastName2.Length == 0 ) 
+            {
+                Console.Write("Podaj nazwisko: ");
+                lastName2 = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Nazwisko: {lastName2}");
+
+            //######################################################
+
+            float a;
+            do
+            {
+              Console.Write("Podaj długość boku: ");
+              a = float.Parse(Console.ReadLine());
+            } while (a <= 0);
+            Console.WriteLine($"Bok kwadratu wynosi: {a}, pole: {a*a}cm^2");
+            
+            //####################################################
+
+            //wzór herona (może być na kartkówce)
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ 2nd_grade/project_05/Program.cs
@@ -0,0 +1,109 @@
+﻿using System.Net.NetworkInformation;
+
+namespace project_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            double a;
+            Console.Write("podaj a: ");
+            a = double.Parse(Console.ReadLine());
+            */
+            /*
+            if (double.TryParse(Console.ReadLine(), out a))
+            {
+                Console.WriteLine(a);
+            }
+            */
+            Console.WriteLine("Wybierz co chcesz zrobić.\n1 - obliczanie pola trójkąta \n2 - obliczanie pola prostokąta \n3 - wyjście");
+            Console.Write("Wybór: ");
+            int choice = int.Parse( Console.ReadLine() );
+            
+            switch (choice)
+            {
+                case 1:
+                    
+                    bool IsCorrect = false;
+
+                    do
+                    {
+                        Console.Write("podaj a: ");
+                        double a;
+
+                        while (!double.TryParse(Console.ReadLine(), out a) || a <= 0)
+                        {
+                            Console.Write("podaj prawidłowo bok a: ");
+                        }
+
+                        Console.Write("podaj b: ");
+                        double b;
+
+                        while (!double.TryParse(Console.ReadLine(), out b) || b <= 0)
+                        {
+                            Console.Write("podaj prawidłowo bok b: ");
+                        }
+
+                        Console.Write("podaj c: ");
+                        double c;
+
+                        while (!double.TryParse(Console.ReadLine(), out c) || c <= 0)
+                        {
+                            Console.Write("podaj prawidłowo bok c: ");
+                        }
+
+                        if (IsTriangle(a, b, c))
+                        {
+                            IsCorrect = true;
+                            double area = CalculateTriangleArea(a, b, c);
+                            Console.OutputEncoding = System.Text.Encoding.Unicode;
+                            Console.WriteLine("Pole trójkąta o bokach {0}, {1}, {2} wynosi: {3:F5}[j\u00b2]", a, b, c, area);
+                            Console.OutputEncoding = System.Text.Encoding.Default;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Z podanych boków nie można stworzyć trójkąta");
+                            Thread.Sleep(2000);
+                            Console.Clear();
+                        }
+                    }
+                    while (!IsCorrect);
+                    break;
+                
+                case 2 :
+                    double a;
+                    double b;
+                    Console.WriteLine("podaj a: ");
+                    a = double.Parse(Console.ReadLine());
+                    Console.WriteLine("podaj b: ");
+                    b = double.Parse(Console.ReadLine());
+                    double area = a * b;
+                    Console.WriteLine($"Pole prostokąta: {area}");
+                    break; 
+                
+                
+                case 3 :
+                    break;
+            }
+            
+            
+
+
+            Console.ReadLine();
+        }
+
+        
+        static bool IsTriangle ( double a, double b, double c)
+        {
+            return a + b > c && b + c > a && a + c > b;
+        }
+
+        static double CalculateTriangleArea( double a, double b, double c)
+        {
+            double p = (a + b + c)/2;
+            return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+        
+    }
+}
