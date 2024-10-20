a3da610f0a378d6952c7f24ccd296ea6568ed924
Nikodem Jokiel <144921134+nikodemJokiel@users.noreply.github.com>
2024-10-20 14:50:14 +0200

    Add files via upload

---
+++ 2nd_grade/project_06/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace project_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Console.WriteLine("podaj 2 liczby: ");
+            try
+            {
+                int x = int.Parse(Console.ReadLine());
+                int y = int.Parse(Console.ReadLine());
+                Console.WriteLine($"iloraz liczb {x}/{y} = {x / y}");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine($"Błąd: liczba powinna być z zakresu od {int.MinValue} do " +
+                    $"{int.MaxValue}");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Błąd: podaj prawidłowe dane");
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine($"Błąd: dzielenie przez 0");
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Błąd: {ex.Message}");
+            }
+            */
+            bool isCorrect = false;
+            do
+            {
+                Console.WriteLine("podaj 2 liczby: ");
+                try
+                {
+                    Console.Write("Podaj x: ");
+                    int x = int.Parse(Console.ReadLine());
+                    Console.Write("Podaj y: ");
+                    int y = int.Parse(Console.ReadLine());
+                    if (y != 0) 
+                    {
+                        Console.Write("\nWynik dzielenia liczb ");
+                    }
+                    ColorMessage($"{x} / {y} = {x / y}", ConsoleColor.Green);
+                    isCorrect = true;
+                   
+                }
+                catch (OverflowException)
+                {
+                    ColorMessage($"Błąd: liczba powinna być z zakresu od {int.MinValue} do {int.MaxValue}", ConsoleColor.Red);
+                }
+                catch (FormatException)
+                {
+                    ColorMessage("Błąd: Błędny format danych", ConsoleColor.Red);
+                }
+                catch (DivideByZeroException)
+                {
+                    ColorMessage("Błąd: dzielenie przez 0", ConsoleColor.Red);
+                }
+
+                catch (Exception ex)
+                {
+                    ColorMessage($"Błąd: {ex.Message}", ConsoleColor.Red);
+                }
+                finally
+                {
+                    Console.WriteLine("\n\nDziękujemy za korzystanie z programu\n\n");
+                }
+            }while (!isCorrect);
+
+            Console.ReadKey();
+
+        }
+        static void ColorMessage(string message, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(message);
+            Console.ResetColor();
+            
+        }
+    }
+    
+}
---
+++ 2nd_grade/project_07/Program.cs
@@ -0,0 +1,117 @@
+﻿
+namespace project_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                Napisz program w C#, który wczytuje od użytkownika 5 liczb całkowitych i zapisuje je w tablicy. Następnie program prosi użytkownika o podanie indeksu tablicy 
+                i wyświetla liczbę znajdującą się pod tym indeksem. Jeśli użytkownik wprowadzi indeks spoza zakresu tablicy, program powinien zgłosić wyjątek 
+                ArgumentOutOfRangeException i wyświetlić odpowiedni komunikat. Jeśli użytkownik wprowadzi nieprawidłowe dane, program powinien zgłosić wyjątek FormatException 
+                i poprosić o ponowne wprowadzenie danych. Program powinien działać w pętli, dopóki użytkownik nie wpisze q. Użyj instrukcji try-catch do obsługi wyjątków
+                Jeśli liczba jest ujemna, program powinien zgłosić wyjątek ArgumentOutOfRangeException i wyświetlić odpowiedni komunikat. 
+                Jeśli użytkownik wprowadzi nieprawidłowe dane, program powinien zgłosić wyjątek FormatException i poprosić o ponowne wprowadzenie liczby. 
+
+             */
+            bool stop = false;
+            do
+            {
+                
+                int[] array = new int[5];
+                bool isCorrect = false;
+                do
+                {
+                    Console.WriteLine("Wprowadź 5 liczb całkowitych: ");
+                    try
+                    {
+                        for (int i = 0; i < array.Length; i++)
+                        
+                        {
+                            Console.Write("Podaj liczbę: ");
+                            array[i] = int.Parse(Console.ReadLine());
+                            if (array[i] < 0)
+                            {
+                                throw new ArgumentOutOfRangeException();
+                            }
+                        }
+                        isCorrect = true;
+                    }
+                    catch (FormatException ex)
+                    {
+                        Console.WriteLine("Nieprawidłowe dane. Wprowadź poprawną liczbę całkowitą.");
+
+                    }
+                    catch (OverflowException)
+                    {
+                        Console.WriteLine($"Nieprawidłowe dane. Liczba poza zakresem <{int.MinValue} ; {int.MaxValue}>");
+                    }
+                    catch (ArgumentOutOfRangeException ex)
+                    {
+                        Console.WriteLine($"{ ex.Message} Podaj liczbę większą od 0");
+                    }
+                    finally
+                    {
+                        Console.WriteLine("Naciśnij q żeby wyjść lub dowolny klawisz by kotynuować");
+                        if (Console.ReadKey(true).Key == ConsoleKey.Q)
+                        {
+                            System.Environment.Exit(0);
+
+                        }
+                    }
+                    
+                }
+                while (!isCorrect);
+
+                bool isCorrect2 = false;
+                do
+                {
+                    try
+                    {
+                        Console.Write("Podaj index: ");
+                        int index = int.Parse(Console.ReadLine());
+                        if (index > 4 || index < 0)
+                        {
+                            throw new ArgumentOutOfRangeException();
+                        }
+
+                        Console.WriteLine($"{array[index]}");
+                        isCorrect = true;
+                    }
+                    catch (ArgumentOutOfRangeException ex)
+                    {
+                        Console.WriteLine($"{ ex.Message} zły index, musi być 0 <= index <= 4");
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("podaj odpowiednie dane");
+                    }
+                    finally
+                    {
+                        Console.WriteLine("Naciśnij q żeby wyjść lub dowolny klawisz by kotynuować");
+                        if (Console.ReadKey(true).Key == ConsoleKey.Q)
+                        {
+                            System.Environment.Exit(0);
+
+                        }
+                    }
+
+                } while (!isCorrect2);
+                
+            } while (!stop) ;
+            
+            
+
+
+
+            /*
+            foreach(int i in array)
+            {
+                Console.WriteLine($"{i}");
+            }
+            */
+            //Console.ReadKey();
+        }
+       
+    }
+}
